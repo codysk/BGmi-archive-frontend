434c86b03fde6748b288f4026f75585e48b4b7af
UmiAo <me@iskywind.com>
2019-04-12 14:36:52 +0800

    [FIN.] Player page layout

--- src/views/Player.vue
+++ src/views/Player.vue
@@ -1,15 +1,44 @@
 <template>
-  <div>
-    <div class="tile double bg-green">
-      <div class="tile-content">
-        <h2>This is an Player page</h2>
-      </div>
-    </div>
-    <div
-      class="container-fluid start-screen h-100"
-      style="overflow: auto;"
-    >
-      <h1 class="start-screen-title">Bangumi.mp4</h1>
+  <div
+    class="container-fluid h-100 player-screen"
+    style="overflow: auto;"
+  >
+    <h1 class="start-screen-title">Bangumi.mp4</h1>
+    <div class="player-container">
+      <div id="dplayer"/>
     </div>
   </div>
 </template>
+
+<style scoped>
+.player-screen {
+  padding-top: 140px;
+}
+.player-container {
+  background-color: white;
+  width: 70%;
+  min-height: 30%;
+  margin: 60px auto 0 auto;
+}
+</style>
+
+<script>
+import 'dplayer/dist/DPlayer.min.css';
+import DPlayer from 'dplayer';
+
+export default {
+  name: 'Player',
+  mounted(){
+    this.$nextTick(() => {
+      const dp = new DPlayer({
+        container: document.getElementById('dplayer'),
+        screenshot: true,
+        autoplay: false,
+        video: {
+            url: '#',
+        }
+      });
+    })
+  }
+}
+</script>
